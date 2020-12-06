515cfd2c85f29d43de88fbc7f3d3516bb627ce85
Antonio Cesar <cezzmartins@gmail.com>
2020-12-06 00:03:08 -0300

    Finalizando html e trocando algumas classes do css

--- slaca2020/js/app.js
+++ slaca2020/js/app.js
@@ -1,1 +1,22 @@
+
+// Habilitar digitação no form de texto
 fildText.document.designMode = "On";     
+
+
+// Função ler mais do Resumo
+
+let status = 0;
+function read(){
+    if(!status){
+        document.getElementById('read-content').style.display = 'inline';
+        document.getElementById('read-dots').style.display = 'none';
+        document.getElementById('read-more').innerHTML = 'Ver menos';
+
+        status = 1;
+    }else{
+        document.getElementById('read-content').style.display = 'none';
+        document.getElementById('read-dots').style.display = 'inline';
+        document.getElementById('read-more').innerHTML = 'Ver Mais';
+        status = 0;
+    }
+}
